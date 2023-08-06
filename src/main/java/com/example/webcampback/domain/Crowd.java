6b87d1c75b7420338ad5b813f0049b9c80565be7
healim01 <22000770@handong.edu>
2023-08-07 01:37:53 +0900

    Update Crowd.java

--- src/main/java/com/example/webcampback/domain/Crowd.java
+++ src/main/java/com/example/webcampback/domain/Crowd.java
@@ -2,6 +2,8 @@ package com.example.webcampback.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
 import lombok.Getter;
 
 @Entity
@@ -9,6 +11,19 @@ import lombok.Getter;
 public class Crowd {
     @Id
     private Long id;
-    private String name;
+    private int fundingId;
+    private String title;
+    private String company;
+    @Lob
+    private String fund_img;
+    private long current_amount;
+    private int progress;
+    private int backers_count;
+    private int goal_amount;
+    private String start_date;
+    private String end_date;
+    private String billing_date;
+    private int left_day;
+    private int like_num;
 
 }

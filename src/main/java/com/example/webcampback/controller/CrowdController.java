6d9fc745b9effd0299c85752857420492121199e
healim01 <22000770@handong.edu>
2023-08-07 01:37:51 +0900

    Update CrowdController.java

--- src/main/java/com/example/webcampback/controller/CrowdController.java
+++ src/main/java/com/example/webcampback/controller/CrowdController.java
@@ -4,13 +4,16 @@ import com.example.webcampback.domain.Crowd;
 import com.example.webcampback.service.CrowdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins="*")
 public class CrowdController {
 
     private final CrowdService crowdService;
@@ -18,5 +21,10 @@ public class CrowdController {
     @GetMapping("/")
     public ResponseEntity<List<Crowd>> getCrowd(){
         return ResponseEntity.ok(crowdService.getCrowd());
+    }
+
+    @GetMapping("/{fundingId}")
+    public ResponseEntity<Crowd> getCrowdOne(@PathVariable("fundingId") int id){
+        return ResponseEntity.ok(crowdService.getCrowdOne(id));
     }
 }
